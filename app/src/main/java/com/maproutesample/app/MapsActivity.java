8d1fe90365aa79afd907828f04ac765685c7502d
Esack <esack@appoets.com>
2018-10-12 16:20:45 +0530

    First commit

---
+++ app/src/main/java/com/maproutesample/app/MapsActivity.java
@@ -0,0 +1,433 @@
+package com.maproutesample.app;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+import com.google.maps.model.TravelMode;
+
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, RoutingListener {
+
+    public static final int REQUEST_CODE_FINE_LOCATION = 1106;
+    private static final int overview = 0;
+    public static int REQUEST_CODE_DELIVERY_SUBMIT = 1105;
+    private final int UPDATE_INTERVAL = 1000;
+    private final int FASTEST_INTERVAL = 900;
+    private final int PLACE_AUTOCOMPLETE_REQUEST_CODE_SOURCE = 1;
+    private final int PLACE_AUTOCOMPLETE_REQUEST_CODE_DEST = 2;
+    private final int REQUEST_CODE_AUTOCOMPLETE = 2;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private GoogleApiClient googleApiClient;
+    private String TAG = "RouteSample";
+    private Location lastLocation;
+    private LocationRequest locationRequest;
+    private LocationCallback mLocationCallback;
+    private TextView txtSource;
+    private TextView txtDestination;
+    private Activity thisActivity;
+    private Intent intent;
+    private double source_lat;
+    private double source_lng;
+    private double dest_lat;
+    private double dest_lng;
+    private String strSourceAddress = "";
+    private String strDestinationAddress = "";
+    private Marker sourceMarker;
+    private Marker destinationMarker;
+    private Marker currentMarker;
+    private int value = 0;
+    private List<LatLng> path;
+    private LatLng sourceLatLng;
+    private LatLng waypointLatLng;
+    private LatLng destLatLng;
+    private ArrayList<Polyline> polylines = new ArrayList<>();
+    private List<LatLng> routes = new ArrayList<>();
+    private boolean exceededTolerance = false;
+    private LatLng currentLatLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        createGoogleApi();
+        thisActivity = this;
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        txtSource = findViewById(R.id.txtSource);
+        txtDestination = findViewById(R.id.txtDestination);
+
+        txtSource.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                        .setTypeFilter(AutocompleteFilter.TYPE_FILTER_NONE)
+                        .setCountry("IN")
+                        .build();
+
+                try {
+                    intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                            .setFilter(typeFilter)
+                            .build(thisActivity);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+                startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE_SOURCE);
+            }
+        });
+
+        txtDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                        .setTypeFilter(AutocompleteFilter.TYPE_FILTER_NONE)
+                        .setCountry("IN")
+                        .build();
+                try {
+                    intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                            .setFilter(typeFilter)
+                            .build(thisActivity);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+                startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE_DEST);
+            }
+        });
+
+    }
+
+    private void createGoogleApi() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    if (!PolyUtil.isLocationOnPath(new LatLng(location.getLatitude(), location.getLongitude()), routes, true, 100)) {
+                        exceededTolerance = true;
+                        drawRoutePath();
+                    }
+
+                    if (value == 0) {
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+                        value++;
+                    }
+                }
+            }
+
+        };
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (checkPermission()) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            askPermission();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getLastKnownLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void getLastKnownLocation() {
+        if (checkPermission()) {
+            mFusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations, this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                lastLocation = location;
+                                currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                                if (currentMarker == null) {
+                                    MarkerOptions markerOptions = new MarkerOptions();
+                                    markerOptions.position(currentLatLng);
+                                    markerOptions.title("My Location");
+                                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.car_marker));
+                                    currentMarker = mMap.addMarker(markerOptions);
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+                                    mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+                                } else {
+                                    currentMarker.setPosition(currentLatLng);
+                                }
+                            }
+                        }
+                    });
+
+            if (lastLocation != null) {
+                startLocationUpdates();
+            } else {
+                startLocationUpdates();
+            }
+        } else askPermission();
+    }
+
+    // Start location Updates
+    private void startLocationUpdates() {
+        Log.i(TAG, "startLocationUpdates()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+        if (checkPermission())
+            mFusedLocationProviderClient.requestLocationUpdates(locationRequest, mLocationCallback, null);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
+    }
+
+    // Check for permission to access Location
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    // Asks for permission
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_DELIVERY_SUBMIT);
+    }
+
+    // Verify user's response of the permission requested
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    getLastKnownLocation();
+
+                } else {
+                    // Permission denied
+                    permissionsDenied();
+                }
+                break;
+            }
+        }
+    }
+
+    // App cannot work without the permissions
+    private void permissionsDenied() {
+        Log.w(TAG, "permissionsDenied()");
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE_SOURCE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                txtSource.setText(place.getAddress());
+                source_lat = place.getLatLng().latitude;
+                source_lng = place.getLatLng().longitude;
+                sourceLatLng = place.getLatLng();
+                strSourceAddress = "" + place.getAddress();
+                drawRoutePath();
+                if (sourceMarker == null) {
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(place.getLatLng());
+                    markerOptions.title("Pickup point");
+                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.pickup));
+                    sourceMarker = mMap.addMarker(markerOptions);
+                } else {
+                    sourceMarker.remove();
+                    sourceMarker.setPosition(place.getLatLng());
+                }
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE_DEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                txtDestination.setText(place.getAddress());
+                strDestinationAddress = "" + place.getAddress();
+                destLatLng = place.getLatLng();
+                dest_lat = place.getLatLng().latitude;
+                dest_lng = place.getLatLng().longitude;
+                drawRoutePath();
+                if (destinationMarker == null) {
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(place.getLatLng());
+                    markerOptions.title("Drop point");
+                    markerOptions.icon(BitmapDescriptorFactory.fromResource(R.mipmap.pickup));
+                    destinationMarker = mMap.addMarker(markerOptions);
+                } else {
+                    destinationMarker.remove();
+                    destinationMarker.setPosition(place.getLatLng());
+                }
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+
+    private void drawRoutePath() {
+        if (!strSourceAddress.equalsIgnoreCase("") && !strDestinationAddress.equalsIgnoreCase("")) {
+            Routing routing = new Routing.Builder()
+                    .travelMode(Routing.TravelMode.DRIVING)
+                    .withListener(this)
+                    .key(getResources().getString(R.string.google_maps_key))
+                    .waypoints(sourceLatLng, currentLatLng, destLatLng)
+                    .build();
+            routing.execute();
+        }
+    }
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex) {
+        if (polylines.size() > 0) {
+            for (Polyline poly : polylines) {
+                poly.remove();
+            }
+        }
+
+        polylines = new ArrayList<>();
+        //add route(s) to the map.
+        for (int i = 0; i < route.size(); i++) {
+            PolylineOptions polyOptions = new PolylineOptions();
+            polyOptions.color(getResources().getColor(R.color.colorPrimary));
+            polyOptions.width(10 + i * 3);
+            polyOptions.addAll(route.get(i).getPoints());
+            Polyline polyline = mMap.addPolyline(polyOptions);
+            polylines.add(polyline);
+            routes = route.get(i).getPoints();
+            Toast.makeText(getApplicationContext(), "Route " + (i + 1) + ": distance - " + route.get(i).getDistanceValue() + ": duration - " + route.get(i).getDurationValue(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
+}
