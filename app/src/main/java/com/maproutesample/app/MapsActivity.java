c033ca62997c00e1802447de023ef0f82d6e234b
Esack <esack@appoets.com>
2018-10-26 16:36:17 +0530

    Moving car with animation

--- app/src/main/java/com/maproutesample/app/MapsActivity.java
+++ app/src/main/java/com/maproutesample/app/MapsActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Handler;
@@ -46,6 +47,7 @@ import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -252,7 +254,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                                     mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
                                     mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
                                 } else {
-                                    currentMarker.setPosition(currentLatLng);
+//                                    currentMarker.setPosition(currentLatLng);
+                                    animateMarker(mMap, currentMarker, currentLatLng);
                                     if (prevLoc != null) {
                                         rotateMarker(currentMarker, prevLoc.bearingTo(location) , mMap);
                                     }
@@ -460,6 +463,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if (t < 1.0) {
                     // Post again 16ms later.
                     handler.postDelayed(this, 16);
+                }
+            }
+        });
+    }
+
+    public static void animateMarker(GoogleMap mMap, final Marker marker, final LatLng toPosition) {
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        Projection proj = mMap.getProjection();
+        Point startPoint = proj.toScreenLocation(marker.getPosition());
+        final LatLng startLatLng = proj.fromScreenLocation(startPoint);
+        final long duration = 1000;
+
+        final Interpolator interpolator = new LinearInterpolator();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                float t = interpolator.getInterpolation((float) elapsed / duration);
+                double lng = t * toPosition.longitude + (1 - t) * startLatLng.longitude;
+                double lat = t * toPosition.latitude + (1 - t) * startLatLng.latitude;
+
+                marker.setPosition(new LatLng(lat, lng));
+
+                if (t < 1.0) {
+                    // Post again 16ms later.
+                    handler.postDelayed(this, 16);
+                } else {
+
                 }
             }
         });
